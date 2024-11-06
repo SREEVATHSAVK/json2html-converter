c4134e5d800b4b1557da969f2bd20311f67d42cc
SREEVATHSAV KYAMA <98406156+SREEVATHSAVK@users.noreply.github.com>
2024-11-06 14:33:33 +0530

    Add files via upload

---
+++ json2html.js
@@ -0,0 +1,32 @@
+export default function json2html(data) {
+    let html = `
+      <table data-user="tabhivm2905@gmail.com">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+    `;
+  
+    data.forEach((item) => {
+      html += `<tr>
+                 <td>${item.Name}</td>
+                 <td>${item.Age}</td>`;
+      if (item.Gender) {
+        html += `<td>${item.Gender}</td>`;
+      } else {
+        html += `<td></td>`;
+      }
+      html += `</tr>`;
+    });
+  
+    html += `
+        </tbody>
+      </table>
+    `;
+  
+    return html;
+  }
